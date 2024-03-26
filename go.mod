0fe145d70d996911dee242c93625ca5c50ef2c06
MikhailFerapontow <mvferapontov@gmail.com>
2024-03-26 15:57:46 +0300

    feat(task1): add custom stemmer

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/MikhailFerapontow/yadro-go
 
 go 1.22.1
+
+require github.com/kljensen/snowball v0.9.0 // indirect
---
+++ task1/stem.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strings"
+
+	"github.com/kljensen/snowball"
+)
+
+type Stemmer struct {
+	stopWordsList []string
+}
+
+func InitStemmer() *Stemmer {
+	/*
+		Оригинальный список стоп-слов из библиотеки snowball мал, поэтому решил взять данный список.
+		Взято отсюда https://countwordsfree.com/stopwords
+	*/
+	file, err := os.ReadFile("stop_words_english.txt")
+
+	if err != nil {
+		log.Fatalf("Error reading stop words file: %s", err.Error())
+	}
+
+	stopWordsList := strings.Split(string(file), "\n")
+
+	// После Split на конце строк остался '\n'. Удаляем его.
+	for i, line := range stopWordsList {
+		stopWordsList[i] = strings.TrimSpace(line) // наверное это можно сделать проще
+	}
+	return &Stemmer{stopWordsList: stopWordsList}
+}
+
+func (s *Stemmer) checkStopWord(target string) bool {
+	for _, stopWord := range s.stopWordsList {
+		if target == stopWord {
+			return true
+		}
+	}
+	return false
+}
+
+func (s *Stemmer) Stem(initialString string) string {
+	words := strings.Fields(initialString)
+	ans := []string{}
+	seenWords := make(map[string]bool)
+
+	for i := range words {
+		stemmed, err := snowball.Stem(words[i], "english", false)
+
+		if err != nil {
+			log.Fatalf("Internal error stemming word: %s", err.Error())
+		}
+
+		// Проверяем является ли слово стоп-словом
+		if s.checkStopWord(stemmed) {
+			continue
+		}
+
+		// Проверям если уже встречали это слово
+		if seenWords[stemmed] {
+			continue
+		}
+
+		seenWords[stemmed] = true
+		ans = append(ans, stemmed)
+	}
+	return strings.Join(ans, " ")
+}
