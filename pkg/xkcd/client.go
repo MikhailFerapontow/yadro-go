80a13e36ee6bfff261fea5b51a3a93ae71d468eb
MikhailFerapontow <mvferapontov@gmail.com>
2024-04-14 18:22:43 +0300

    fix(client): change channel for WaitGroup

--- pkg/xkcd/client.go
+++ pkg/xkcd/client.go
@@ -29,57 +29,58 @@ func NewCLient(url string) *Client {
 }
 
 func (c *Client) GetComics(ctx context.Context, limit int, existing_comics map[int]bool) ([]models.ResponseComic, error) {
-	max_id, err := c.GetLastId(ctx)
+	maxId, err := c.GetLastId(ctx)
 	if err != nil {
 		log.Printf("Error getting last comic id: %s", err)
 		return nil, err
 	}
 
-	log.Printf("Found last comic with id = %d", max_id)
+	log.Printf("Found last comic with id = %d", maxId)
 
 	var comics []models.ResponseComic
 	mu := sync.Mutex{}
-	idchannel := make(chan int, max_id)
-	finishch := make(chan int, max_id)
+	wg := sync.WaitGroup{}
+	idchannel := make(chan int, maxId)
 
 	for w := 1; w <= limit; w++ {
-		go func(ctx context.Context, idchannel <-chan int, finishch chan<- int) {
+		go func() {
 			for id := range idchannel {
-				if existing_comics[id] {
-					finishch <- 1
-					continue
-				}
+				wg.Add(1)
 
-				comic, err := c.getComicById(ctx, id)
-				if err != nil {
-					log.Printf("Error getting comic with id = %d: %s", id, err)
-					finishch <- 1
-					continue
+				select {
+				case <-ctx.Done():
+					wg.Done()
+					return
+				default:
+					if existing_comics[id] {
+						wg.Done()
+						continue
+					}
+
+					comic, err := c.getComicById(ctx, id)
+					if err != nil {
+						log.Printf("Error getting comic with id = %d: %s", id, err)
+						wg.Done()
+						continue
+					}
+					mu.Lock()
+					comics = append(comics, comic)
+					mu.Unlock()
+					wg.Done()
 				}
-				mu.Lock()
-				comics = append(comics, comic)
-				mu.Unlock()
-				finishch <- 1
 			}
-		}(ctx, idchannel, finishch)
+		}()
 	}
 
-	for j := 1; j <= max_id; j++ {
+	for j := 1; j <= maxId; j++ {
 		idchannel <- j
 	}
 	close(idchannel)
 
-	for a := 1; a <= max_id; a++ {
-		select {
-		case <-ctx.Done():
-			return comics, ctx.Err()
-		default:
-			<-finishch
-		}
-	}
-	close(finishch)
+	wg.Wait()
+
 	log.Printf("Finished fetching comics")
-	return comics, nil
+	return comics, ctx.Err()
 }
 
 func (c *Client) getComicById(ctx context.Context, id int) (models.ResponseComic, error) {
@@ -87,27 +88,23 @@ func (c *Client) getComicById(ctx context.Context, id int) (models.ResponseComic
 
 	req, err := http.NewRequestWithContext(ctx, "GET", query, nil)
 	if err != nil {
-		log.Printf("Error getting comic with id = %d: %s", id, err)
 		return models.ResponseComic{}, err
 	}
 
 	resp, err := c.httpClient.Do(req)
 	if err != nil {
-		log.Printf("Error getting comic with id = %d: %s", id, err)
 		return models.ResponseComic{}, err
 	}
 
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		log.Printf("Error getting comic id = %d: %s", id, resp.Status)
 		return models.ResponseComic{}, err
 	}
 
 	var comic models.ResponseComic
 	err = json.NewDecoder(resp.Body).Decode(&comic)
 	if err != nil {
-		log.Printf("Error unmarshaling comic id = %d: %s", id, err)
 		return models.ResponseComic{}, err
 	}
 
